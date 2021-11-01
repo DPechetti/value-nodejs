6d6b5313fc67ba89eff17a38e5a44c99d07722c3
Donovan Pechetti <dovi_pf@hotmail.com>
2021-11-01 20:13:44 -0300

    router created

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
+import "reflect-metadata";
+
 import express from 'express';
+
+import './database'
+import { router } from "./routes";
 
 const port = 3333;
 const app = express();
+
+app.use(express.json())
+app.use('/api', router)
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
