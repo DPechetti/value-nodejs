4699722dba767f6c7859c4fb151f53415cdbe1a8
Donovan Pechetti <dovi_pf@hotmail.com>
2021-11-02 10:20:38 -0300

    set admin default value to false

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -10,7 +10,7 @@ interface IUserRequest {
 }
 
 class CreateUserService {
-  async execute({ name, email, password, admin }: IUserRequest) {
+  async execute({ name, email, password, admin = false }: IUserRequest) {
     const usersRepository = getCustomRepository(UsersRepository)
 
     if (!email) throw new Error("Incorrect email")
