cdfde74a7000e5dffd087b3538ff823171df2ff4
Donovan Pechetti <dovi_pf@hotmail.com>
2021-11-02 11:43:00 -0300

    ensure authenticated middleware created

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,24 @@
+import { NextFunction, request, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IVerifyResponse {
+  sub: string;
+}
+
+export function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
+  const authToken = req.headers.authorization // hardcoded for now
+
+  if (!authToken) return res.status(401).json({ error: 'Invalid token' })
+
+  const [, token] = authToken.split(' ')
+
+  try {
+    const { sub } = verify(token,'Batatinha') as IVerifyResponse
+
+    request.user_id = sub
+    
+    return next()
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token' })
+  }
+}
