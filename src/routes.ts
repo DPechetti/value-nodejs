3a2022cb9b8e1a69a9eda63c92e3c678f19f675f
Donovan Pechetti <dovi_pf@hotmail.com>
2021-11-02 12:43:35 -0300

    routes to list user compliments sent and received created

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 
+import { ListUserReceivedComplimentController } from "./controllers/ListUserReceivedComplimentController";
+import { ListUserSentComplimentController } from "./controllers/ListUserSentComplimentController";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { CreateUserController } from "./controllers/CreateUserController";
@@ -9,6 +11,8 @@ import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router()
 
+const listUserReceivedComplimentController = new ListUserReceivedComplimentController();
+const listUserSentComplimentController = new ListUserSentComplimentController()
 const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
 const createUserController = new CreateUserController()
@@ -18,5 +22,7 @@ router.post('/users', createUserController.handle)
 router.post('/login', authenticateUserController.handle)
 router.post('/tags', ensureAuthenticated, ensureAdmin, createTagController.handle)
 router.post('/compliments', ensureAuthenticated, createComplimentController.handle)
+router.get('/compliments/sent', ensureAuthenticated, listUserSentComplimentController.handle)
+router.get('/compliments/received', ensureAuthenticated, listUserReceivedComplimentController.handle)
 
 export { router }
