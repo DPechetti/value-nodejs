b9258e697ce58732b6a2991bea5195e9251005d1
Donovan Pechetti <dovi_pf@hotmail.com>
2021-11-02 06:50:04 -0300

    authenticate user route created

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router()
 
+const authenticateUserController = new AuthenticateUserController()
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 
 router.post('/users', createUserController.handle)
+router.post('/login', authenticateUserController.handle)
 router.post('/tags', ensureAdmin, createTagController.handle)
 
 export { router }
