6895fbfd76b12f004fd401c6a36aecae5be5f1a6
Donovan Pechetti <dovi_pf@hotmail.com>
2021-11-02 10:17:13 -0300

    compliments route created

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,21 @@
 import { Router } from "express";
+
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
-import { CreateTagController } from "./controllers/CreateTagController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router()
 
 const authenticateUserController = new AuthenticateUserController()
+const createComplimentController = new CreateComplimentController()
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 
 router.post('/users', createUserController.handle)
 router.post('/login', authenticateUserController.handle)
 router.post('/tags', ensureAdmin, createTagController.handle)
+router.post('/compliments', createComplimentController.handle)
 
 export { router }
